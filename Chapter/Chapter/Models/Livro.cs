090b7b43435708c4981146be2b837e3760941243
JaykeBentes <jakebentes@hotmail.com>
2023-07-31 20:24:06 -0400

    Atualização de AulaER1

---
+++ Chapter/Chapter/Models/Livro.cs
@@ -0,0 +1,11 @@
+﻿namespace Chapter.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public int QuantidadePaginas { get;}
+
+        public bool Disponivel { get; set; }
+    }
+}
